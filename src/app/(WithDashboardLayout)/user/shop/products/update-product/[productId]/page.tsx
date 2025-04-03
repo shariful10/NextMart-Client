812d43681d231af2e417cb46d17e7e535e6c94b5
Md. Shariful Islam <freelancershariful10@gmail.com>
2025-04-03 21:56:07 +0600

    Create Dynamic Input Fields & Add Product

---
+++ src/app/(WithDashboardLayout)/user/shop/products/update-product/[productId]/page.tsx
@@ -0,0 +1,20 @@
+import UpdateProductForm from "@/components/modules/shop/product/UpdateProductForm";
+import { getSingleProduct } from "@/services/product";
+
+const UpdateProductPage = async ({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}) => {
+  const { productId } = await params;
+
+  const { data: product } = await getSingleProduct(productId);
+
+  return (
+    <div className="flex justify-center items-center">
+      <UpdateProductForm product={product} />
+    </div>
+  );
+};
+
+export default UpdateProductPage;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 export * from "./brand";
 export * from "./category";
+export * from "./product";
 export * from "./sidebar";
 export * from "./user";
