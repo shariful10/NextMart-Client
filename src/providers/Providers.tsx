ae37b1ce913d4a56c930b998b4d323a14f501057
Md. Shariful Islam <freelancershariful10@gmail.com>
2025-04-06 00:52:16 +0600

    Connecting Redux with NextMart App

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -2,9 +2,14 @@
 
 import UserProvider from "@/context/UserContext";
 import { ReactNode } from "react";
+import StoreProvider from "./StoreProvider";
 
 const Providers = ({ children }: { children: ReactNode }) => {
-	return <UserProvider>{children}</UserProvider>;
+	return (
+		<UserProvider>
+			<StoreProvider>{children}</StoreProvider>
+		</UserProvider>
+	);
 };
 
 export default Providers;
