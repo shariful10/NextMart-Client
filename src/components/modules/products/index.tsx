9e8c6c9ad9fa02cfe57ccfab396d6d8c00fb0d86
shariful10 <freelancershariful10@gmail.com>
2025-04-13 02:43:53 +0600

    Filter Products by Multiple Fields & Conclusion

--- src/components/modules/products/index.tsx
+++ src/components/modules/products/index.tsx
@@ -4,12 +4,12 @@ import FilterSidebar from "./filterSidebar";
 
 const Products = ({ products }: { products: TProduct[] }) => {
 	return (
-		<div className="flex gap-8 my-10">
+		<div className="flex gap-4 my-10">
 			<div className="w-full max-w-sm border-2 border-gray-200 rounded-xl">
 				<FilterSidebar />
 			</div>
 			<div>
-				<div className="grid grid-cols-4 gap-12">
+				<div className="grid grid-cols-4 gap-4">
 					{products?.map((product: TProduct, idx: number) => (
 						<ProductCard key={idx} product={product} />
 					))}
--- src/services/product/index.ts
+++ src/services/product/index.ts
@@ -6,11 +6,30 @@ import { revalidateTag } from "next/cache";
 export const getAllProducts = async (
 	page?: string,
 	limit?: string,
-	query?: Record<string, string>
+	query?: { [key: string]: string | string[] | undefined }
 ) => {
+	const params = new URLSearchParams();
+
+	if (query?.price) {
+		params.append("minPrice", "0");
+		params.append("maxPrice", query?.price.toString());
+	}
+
+	if (query?.category) {
+		params.append("categories", query?.category.toString());
+	}
+
+	if (query?.brand) {
+		params.append("brands", query?.brand.toString());
+	}
+
+	if (query?.rating) {
+		params.append("ratings", query?.rating.toString());
+	}
+
 	try {
 		const res = await fetch(
-			`${process.env.NEXT_PUBLIC_BASE_API}/product?page=${page}&limit=${limit}`,
+			`${process.env.NEXT_PUBLIC_BASE_API}/product?page=${page}&limit=${limit}&${params}`,
 			{
 				next: {
 					tags: ["PRODUCT"],
