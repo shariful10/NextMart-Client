d114a6b1030d1e233dcc305a39af33473b70e547
shariful10 <freelancershariful10@gmail.com>
2025-04-13 02:02:57 +0600

    Implement Functional Product Filter Options

--- src/components/modules/products/filterSidebar/index.tsx
+++ src/components/modules/products/filterSidebar/index.tsx
@@ -1,95 +1,163 @@
 "use client";
 
-import { Card, CardContent } from "@/components/ui/card";
-import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Slider } from "@/components/ui/slider";
-import { useState } from "react";
+import { currencyFormatter } from "@/lib/currencyFormatter";
+import { getAllBrands } from "@/services/brand";
+import { getAllCategories } from "@/services/category";
+import { Star } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 const FilterSidebar = () => {
-	const [price, setPrice] = useState(50);
+	const router = useRouter();
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
 
-	const productTypes = [
-		"Laptop & Accessories",
-		"Computers Pc",
-		"Speakers & Headset",
-		"Keyboards & Mouse",
-		"Camera",
-		"Video Recording",
-		"Tablet",
-		"Table Lights",
-	];
+	const [price, setPrice] = useState([0]);
+	const [brands, setBrands] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [categories, setCategories] = useState([]);
 
-	const brands = ["HP", "Apple", "Dell", "Asus", "Canon"];
-	const ratings = [5, 4, 3, 2, 1];
-	const availability = ["In Stock", "Pre Order", "Upcoming"];
+	useEffect(() => {
+		const fetchData = async () => {
+			setIsLoading(true);
+			try {
+				const [{ data: categoriesData }, { data: brandsData }] =
+					await Promise.all([getAllCategories(), getAllBrands()]);
+				setCategories(categoriesData);
+				setBrands(brandsData);
+			} catch (error: any) {
+				console.error(error);
+				toast.error("Failed to fetch filters");
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	const handleSearchQuery = (query: string, value: string | number) => {
+		const params = new URLSearchParams(searchParams.toString());
+
+		params.set(query, value.toString());
+
+		router.push(`${pathname}?${params.toString()}`, {
+			scroll: false,
+		});
+	};
 
 	return (
-		<Card className="p-4 rounded-2xl shadow-md w-72">
-			<CardContent>
-				<h2 className="text-lg font-semibold mb-4">Filter By Price</h2>
-				<div className="flex gap-2 mb-4">
-					<input
-						type="text"
-						placeholder="Min"
-						className="border rounded px-2 py-1 w-full"
-					/>
-					<input
-						type="text"
-						placeholder="Max"
-						className="border rounded px-2 py-1 w-full"
-					/>
+		<div className="p-6 bg-white rounded-lg border-2 border-gray-100 w-[275px]">
+			<div className="flex justify-between items-center mb-6">
+				<h2 className="text-xl font-semibold">Filter</h2>
+				{searchParams.toString().length > 0 && (
+					<Button
+						onClick={() => {
+							router.push(`${pathname}`, {
+								scroll: false,
+							});
+						}}
+						size="sm"
+						className="bg-black hover:bg-gray-700 ml-5 cursor-pointer"
+					>
+						Clear Filters
+					</Button>
+				)}
+			</div>
+			{/* Filter by Price */}
+			<div className="mb-6">
+				<h2 className="text-lg font-semibold mb-4">Price</h2>
+				<div className="flex items-center justify-between text-sm mb-2">
+					<span>{currencyFormatter(0)}</span>
+					<span>{currencyFormatter(50000)}</span>
 				</div>
 				<Slider
-					defaultValue={[price]}
-					max={100}
-					onValueChange={(val) => setPrice(val[0])}
+					max={50000}
+					step={1}
+					onValueChange={(value) => {
+						setPrice(value);
+						handleSearchQuery("price", value[0]);
+					}}
+					className="w-full"
 				/>
-				<p className="mt-2">${price}</p>
-
-				<h2 className="text-lg font-semibold mt-6">Product Types</h2>
-				<ul className="space-y-2 mt-2">
-					{productTypes.map((type, index) => (
-						<li key={index} className="flex gap-2">
-							<Checkbox />
-							<span className="-mt-1">{type}</span>
-						</li>
+				<p className="text-sm mt-2">
+					Selected Price: {currencyFormatter(price[0])}
+				</p>
+			</div>
+			{/* Product Types */}
+			<div className="mb-6">
+				<h2 className="text-lg font-semibold mb-4">Product Category</h2>
+				{!isLoading && (
+					<RadioGroup className="space-y-2">
+						{categories?.map((category: { _id: string; name: string }) => (
+							<div key={category._id} className="flex items-center space-x-2">
+								<RadioGroupItem
+									onClick={() => handleSearchQuery("category", category._id)}
+									value={category._id}
+									id={category._id}
+								/>
+								<Label
+									htmlFor={category._id}
+									className="text-gray-500 font-light"
+								>
+									{category.name}
+								</Label>
+							</div>
+						))}
+					</RadioGroup>
+				)}
+			</div>
+			{/* Brands */}
+			<div className="mb-6">
+				<h2 className="text-lg font-semibold mb-4">Brands</h2>
+				{!isLoading && (
+					<RadioGroup className="space-y-2">
+						{brands?.map((brand: { _id: string; name: string }) => (
+							<div key={brand._id} className="flex items-center space-x-2">
+								<RadioGroupItem
+									onClick={() => handleSearchQuery("brand", brand._id)}
+									value={brand._id}
+									id={brand._id}
+								/>
+								<Label htmlFor={brand._id} className="text-gray-500 font-light">
+									{brand.name}
+								</Label>
+							</div>
+						))}
+					</RadioGroup>
+				)}
+			</div>
+			{/* Rating */}
+			<div className="mb-6">
+				<h2 className="text-lg font-semibold mb-4">Rating</h2>
+				<RadioGroup className="space-y-3">
+					{[5, 4, 3, 2, 1].map((rating) => (
+						<div key={rating} className="flex items-center space-x-2">
+							<RadioGroupItem
+								onClick={() => handleSearchQuery("rating", rating)}
+								value={`${rating}`}
+								id={`rating-${rating}`}
+							/>
+							<Label htmlFor={`rating-${rating}`} className="flex items-center">
+								{Array.from({ length: 5 }, (_, i) => (
+									<Star
+										size={18}
+										key={i}
+										fill={i < rating ? "orange" : "lightgray"}
+										stroke={i < rating ? "orange" : "lightgray"}
+									/>
+								))}
+							</Label>
+						</div>
 					))}
-				</ul>
-
-				<h2 className="text-lg font-semibold mt-6">Brands</h2>
-				<ul className="space-y-2 mt-2">
-					{brands.map((brand, index) => (
-						<li key={index} className="flex items-center gap-2">
-							<Checkbox />
-							<span>{brand}</span>
-						</li>
-					))}
-				</ul>
-
-				<h2 className="text-lg font-semibold mt-6">Rating</h2>
-				<ul className="space-y-2 mt-2">
-					{ratings.map((rating, index) => (
-						<li key={index} className="flex items-center gap-2">
-							<Checkbox />
-							<span className="text-yellow-500">
-								{"★".repeat(rating)}
-								{"☆".repeat(5 - rating)}
-							</span>
-						</li>
-					))}
-				</ul>
-
-				<h2 className="text-lg font-semibold mt-6">Availability</h2>
-				<ul className="space-y-2 mt-2">
-					{availability.map((status, index) => (
-						<li key={index} className="flex items-center gap-2">
-							<Checkbox />
-							<span>{status}</span>
-						</li>
-					))}
-				</ul>
-			</CardContent>
-		</Card>
+				</RadioGroup>
+			</div>
+		</div>
 	);
 };
 
